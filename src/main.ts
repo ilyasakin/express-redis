bdb63e5bc67c6d7d2c97286856a1bc630f149231
İlyas Akın <ashnwor@gmail.com>
2023-09-10 20:30:49 +0300

    Add getUsersWithoutCache endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import {sleep} from "./helper";
 
 const app = express();
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + TypeScript!");
+interface IUser {
+    name: string;
+    age: number;
+    email: string;
+}
+
+const users: IUser[] = [
+    {
+        name: "John",
+        age: 25,
+        email: "john@randomemalilproviderthatcertainlydoesntmatter.com"
+    }
+];
+
+app.get("/getUsersWithoutCache", async (_, res) => {
+   // Artificial delay to simulate a slow API
+    await sleep(5000);
+    res.json(users);
 });
 
+
 ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000...")
+    console.log("Server is listening on port 3000...")
 );
